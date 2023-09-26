fda916703a17c2bf8352688c83f1298d464c9257
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2023-09-26 15:47:41 +0300

    Update vertica_useful_scripts.sql

--- vertica_useful_scripts.sql
+++ vertica_useful_scripts.sql
@@ -1,1 +1,11 @@
 SELECT CLOSE_ALL_SESSIONS(); -- close all sessions except during session
+
+SELECT AUDIT('public.table_name', 'table', 0, 100);
+SELECT ANALYZE_STATISTICS('public.table_name');
+
+-- CHECK ALL ROS CONTAINERS
+SELECT * FROM storage_containers
+WHERE projection_name = 'table_name' and SCHEMA_name = 'schema_name';
+-- SELECT EXECTLY 1 CONTAINER (CHOOSE FROM PREVIOUS RESULT)
+SELECT * FROM table_name.schema_name 
+WHERE lead_storage_oid() = '45035996283468437';
