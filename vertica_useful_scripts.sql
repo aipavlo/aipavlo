5acaf3d2ffd74b01aed7b0c21c6a2e4a0eb1c47c
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2023-09-29 14:46:06 +0300

    Update vertica_useful_scripts.sql

--- vertica_useful_scripts.sql
+++ vertica_useful_scripts.sql
@@ -22,5 +22,15 @@ SELECT * FROM v_monitor.query_requests WHERE user_name = 'dbadmin' AND is_execut
 -- CHECK ENABLED AND DISABLED CONSTRAINTS
 SELECT CONSTRAINT_NAME, CONSTRAINT_TYPE, IS_ENABLED FROM V_CATALOG.TABLE_CONSTRAINTS
 WHERE TABLE_NAME = 'table_name' AND CONSTRAINT_SCHEMA_ID = (SELECT SCHEMA_ID FROM V_CATALOG.SCHEMATA WHERE SCHEMA_NAME = 'schema_name');
+
+-- ADD CONSTRAINT
+ALTER TABLE schema_name.table_name ADD CONSTRAINT constraint_name PRIMARY KEY (column_name1, column_name2, ...) ENABLED;
 -- ENABLE CONSTRAINT
 ALTER TABLE schema_name.table_name ALTER CONSTRAINT table_name_PK ENABLED;
+
+-- 100 BIGGEST TABLES IN VERTICA DB
+SELECT anchor_table_schema, anchor_table_name, SUM(used_bytes) AS total_used_bytes
+FROM v_monitor.column_storage
+GROUP BY anchor_table_name, anchor_table_schema
+ORDER BY total_used_bytes DESC
+LIMIT 100;
