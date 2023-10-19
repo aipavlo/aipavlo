0dba6337f69e6c56fb0e20e71f8ff3eeab17f80e
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2023-10-19 13:50:57 +0300

    Create spine_date.sql

---
+++ spine_date.sql
@@ -0,0 +1,7 @@
+CREATE TABLE core_3.spine_date (
+    id INTEGER,
+    yyyymm INTEGER
+);
+
+INSERT INTO core_3.spine_date(id, yyyymm) values (1, 200001);
+INSERT INTO core_3.spine_date select MAX(ID)+1 AS ID, TO_NUMBER(to_char(add_months(to_date(to_char(max(yyyymm)), 'yyyymm') , 1 ), 'YYYYMM')) AS YYYYMM from core_3.spine_date;
