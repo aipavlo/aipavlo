0c07a8f300011ee80a624bce2a3e1437388eaba7
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2023-10-19 13:51:43 +0300

    Update spine_date.sql

--- spine_date.sql
+++ spine_date.sql
@@ -3,5 +3,5 @@ CREATE TABLE public.spine_date (
     yyyymm INTEGER
 );
 
-INSERT INTO core_3.spine_date(id, yyyymm) values (1, 200001);
-INSERT INTO core_3.spine_date select MAX(ID)+1 AS ID, TO_NUMBER(to_char(add_months(to_date(to_char(max(yyyymm)), 'yyyymm') , 1 ), 'YYYYMM')) AS YYYYMM from core_3.spine_date;
+INSERT INTO public.spine_date(id, yyyymm) values (1, 200001);
+INSERT INTO public.spine_date select MAX(ID)+1 AS ID, TO_NUMBER(to_char(add_months(to_date(to_char(max(yyyymm)), 'yyyymm') , 1 ), 'YYYYMM')) AS YYYYMM from public.spine_date;
