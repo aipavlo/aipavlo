d51257de75dc1c8ab699ad816c443eb87e163f79
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2023-11-16 16:35:35 +0300

    Update useful_scripts_bash.sh

--- useful_scripts_bash.sh
+++ useful_scripts_bash.sh
@@ -2,3 +2,8 @@
  zcat dwh.csv.gz | head -n 1
 
  
+### VSQL ###
+# stop on error
+\set ON_ERROR_STOP on
+# Executes a shell command and returns the output to vsql.
+vsql -A -t -d your_database -U your_user -h your_host -w your_password -f /file_to_import/adhoc.sql -o /file_to_import/result.txt -- execute script and have all output in file withou header and footer
