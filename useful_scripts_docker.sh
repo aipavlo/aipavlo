9128623b8ef30f9090d3d571e6fcf7ff80d6781e
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2023-11-27 15:47:55 +0300

    Create useful_scripts_docker.sh

---
+++ useful_scripts_docker.sh
@@ -0,0 +1,4 @@
+# import to docker
+docker cp /home/debian/file_to_import/dwh.csv.gz vert-ce:/file_to_import/dwh.csv.gz
+# export from docker
+docker cp vert-ce:/file_to_export/dwh.csv.gz  /home/debian/file_to_export/dwh.csv.gz
