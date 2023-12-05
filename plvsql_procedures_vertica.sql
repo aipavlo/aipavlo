440a8f223f97cd3b983e2831e3bd19434d744f2c
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2023-12-05 10:33:37 +0300

    Update plvsql_procedures_vertica.sql

--- plvsql_procedures_vertica.sql
+++ plvsql_procedures_vertica.sql
@@ -8,7 +8,7 @@ DECLARE
 	SELECT 
     'INSERT INTO metadata.license_usage (metadata_id, schema_name, table_name, column_name, column_audit) SELECT (SELECT id FROM metadata.sessions WHERE session_id = (SELECT session_id FROM current_session)) as metadata_id,'||table_schema||' AS SCHEMA_NAME, '||table_name||' as TABLE_NAME, '||column_name||' AS COLUMN_NAME, (SELECT SUM(AUDIT_LENGTH('||column_name||')) FROM '||table_schema||'.'||table_name||') AS AUDIT_SUM ;'
     FROM v_catalog.columns
-    WHERE table_schema IN ('core_5') ;
+    WHERE table_schema IN ('schema_name') ;
 	query_ varchar(65000);
 BEGIN
 	FOR query_ IN CURSOR c LOOP
