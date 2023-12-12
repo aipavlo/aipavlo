5aa9e54635a18b340896e398c35c4a05c7982921
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2023-12-12 16:59:50 +0300

    Update useful_scripts_awscli.sh

--- useful_scripts_awscli.sh
+++ useful_scripts_awscli.sh
@@ -10,6 +10,9 @@ aws s3 ls
 # check objects in bucket
 aws s3 ls s3://your-bucket-name
 
+# move all files from one folder to another inside one bucket
+aws s3 mv s3://bucket-name/source-folder/ s3://bucket-name/destination-folder/ --recursive
+
 # create folder
 aws s3api put-object --bucket your-bucket-name --key folder-name/
 aws s3api put-object --bucket your-bucket-name --key folder-name/new-folder-name/
@@ -20,5 +23,5 @@ aws s3 rm s3://your-bucket-name/folder-name/
 # check acl
 aws s3api get-bucket-acl --bucket your-bucket-name
 
-# get size of folder
+# get size of all files inside folder
 aws s3 ls --summarize --human-readable --recursive s3://bucket-name/folder-name/ | grep -E 'Total Size' | awk '{print $3, $4}'
