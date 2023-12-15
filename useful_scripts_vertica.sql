a4f6cb4ac238fe13f9822684d07d54d934f58609
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2023-12-15 12:11:57 +0300

    Update useful_scripts_vertica.sql

--- useful_scripts_vertica.sql
+++ useful_scripts_vertica.sql
@@ -62,6 +62,9 @@ WHERE lead_storage_oid() = '45035996283468437';
 SELECT * FROM v_catalog.user_procedures;
 -- CHECK ALL USERS
 SELECT user_name, resource_pool, all_roles, is_super_user, is_locked, lock_time FROM v_catalog.users;
+CREATE USER etl_vsql;
+ALTER USER ExistingUserDemo IDENTIFIED BY 'newpassword';
+
 
 SELECT EXPORT_OBJECTS( '', 'schema_name.table_name') ; -- export DDL
 
