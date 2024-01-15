f6e4a6caa4d1c414288781f60f870c18087c729a
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-01-15 15:50:34 +0300

    Update useful_scripts_bash.sh

--- useful_scripts_bash.sh
+++ useful_scripts_bash.sh
@@ -8,6 +8,16 @@
 # Executes a shell command and returns the output to vsql.
 vsql -A -t -d your_database -U your_user -h your_host -w your_password -f /file_to_import/adhoc.sql -o /file_to_import/result.txt -- execute script and have all output in file without header and footer
 
+# define variable with query result
+CHECK_CONDITION="SELECT (COUNT(DISTINCT batch_id) > 1)::integer FROM test_schema.test_table;"
+CHECK_RESULT=$(/opt/vertica/bin/vsql -A -t -X -d $VERTICA_DB -U $ETL_VSQL_USER -c "$CHECK_CONDITION")
+if [ $CHECK_RESULT -eq 1 ]
+then
+ echo 1
+else
+ echo 0
+fi
+
 # check hostname
 hostname
 # check public IP
