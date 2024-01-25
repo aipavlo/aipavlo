342ebaffd4b630d8c8c8bf4f99ece27dcd87d4a8
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-01-25 09:49:49 +0300

    Update useful_scripts_postgres.sql

--- useful_scripts_postgres.sql
+++ useful_scripts_postgres.sql
@@ -6,3 +6,21 @@ WITH deleted_rows AS (
 )
 INSERT INTO table2
 SELECT * FROM deleted_rows;
+
+-- CHECK BIGGEST TABLES
+SELECT
+  schema_name,
+  relname,
+  pg_size_pretty(table_size) AS size,
+  table_size
+FROM (
+       SELECT
+         pg_catalog.pg_namespace.nspname           AS schema_name,
+         relname,
+         pg_relation_size(pg_catalog.pg_class.oid) AS table_size
+       FROM pg_catalog.pg_class
+         JOIN pg_catalog.pg_namespace ON relnamespace = pg_catalog.pg_namespace.oid
+     ) t
+WHERE schema_name NOT LIKE 'pg_%'
+ORDER BY table_size desc
+;
