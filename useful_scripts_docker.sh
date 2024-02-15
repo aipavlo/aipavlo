b1f08c7cb2a8ed4726a6eb52a82c32ea285b2f2c
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-02-15 13:54:28 +0300

    Update useful_scripts_docker.sh

--- useful_scripts_docker.sh
+++ useful_scripts_docker.sh
@@ -1,5 +1,8 @@
 # check volumes
 docker volume ls
+# check volumes
+docker volume inspect <volume_name>
+
 # import to docker
 docker cp /home/debian/file_to_import/dwh.csv.gz vert-ce:/file_to_import/dwh.csv.gz
 # export from docker
