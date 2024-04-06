0a935d1de2304a3164b4ae0d2f2b375c9fd37153
Alex Pavlov <122205929+aipavlo@users.noreply.github.com>
2024-04-06 13:36:15 +0300

    Update extract_oracle.py

--- extract_oracle.py
+++ extract_oracle.py
@@ -8,7 +8,6 @@ from paramiko import SSHClient
 from scp import SCPClient
 
 db_connect = cx_Oracle.connect('user/pass@hostname:port/service_name')
-sql = "SELECT * FROM your_table WHERE DT BETWEEN :start_day AND :end_day"
 start_date = datetime.date(2024, 1, 1)
 end_date = datetime.date(2024, 12, 31)
 
@@ -19,13 +18,14 @@ def push_to_server(filename, hostname, username, password):
     with SCPClient(ssh.get_transport()) as scp:
         scp.put(filename + '.zip')
 
-def export_to_csv(db_connect, sql, start_date, end_date):
+def export_to_csv(db_connect, table_name, start_date, end_date):
+    sql = f"SELECT * FROM {table_name} WHERE DT BETWEEN :start_day AND :end_day"
     # Connect to the database
     with db_connect as connection:
         with connection.cursor() as cursor:
             for single_date in (start_date + datetime.timedelta(n) for n in range(int ((end_date - start_date).days))):
                 formatted_day = single_date.strftime("%Y%m%d")
-                filename = f"schema.table.{formatted_day}.csv"
+                filename = f"{table_name}.{formatted_day}.csv"
                 with open(filename, "w", newline="", encoding="utf-8") as csvfile:
                     writer = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
                     
@@ -49,4 +49,4 @@ def export_to_csv(db_connect, sql, start_date, end_date):
                 # Push the file to another server
                 push_to_server(filename, 'hostname', 'username', 'password')
 
-export_to_csv(db_connect, sql, start_date, end_date)
+export_to_csv(db_connect, 'your_table', start_date, end_date)
